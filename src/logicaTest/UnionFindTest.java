49f1993a8bf47a6d6c0845bbe18299f29d6c5e84
Matias Fauda <matiasfauda@gmail.com>
2025-10-01 14:38:20 -0300

    Correccion test unionFinds

--- src/logicaTest/UnionFindTest.java
+++ src/logicaTest/UnionFindTest.java
@@ -51,28 +51,6 @@ public class UnionFindTest {
         assertTrue(uf.estanConectados(u1, u3));
     }
 
-    @Test
-    public void unionByRankMantieneEstructuraEquilibrada() {
-        Usuario u1 = new Usuario("Ana", 3, 3, 3, 3);
-        Usuario u2 = new Usuario("Luis", 4, 4, 4, 4);
-        Usuario u3 = new Usuario("Marta", 5, 5, 5, 5);
-        List<Usuario> usuarios = Arrays.asList(u1, u2, u3);
-
-        UnionFind uf = new UnionFind(usuarios);
-
-        uf.unir(u1, u2);
-        Usuario raiz1 = uf.encontrarRaiz(u1);
-        Usuario raiz2 = uf.encontrarRaiz(u2);
-
-        // Despues de unir, u1 y u2 comparten la misma raiz
-        assertEquals(raiz1, raiz2);
-
-        uf.unir(u1, u3);
-        Usuario raiz3 = uf.encontrarRaiz(u3);
-
-        // Ahora los tres deben compartir raiz
-        assertEquals(raiz1, raiz3);
-    }
 
     @Test
     public void estanConectadosDetectaCorrectamente() {
